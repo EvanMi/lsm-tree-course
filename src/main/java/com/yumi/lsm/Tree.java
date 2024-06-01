7eaf77fa0f06cc999fe50cfa0928e9cbb70a5b5e
mipengcheng3 <mipengcheng3@jd.com>
2024-06-01 18:27:27 +0800

    完成冷mem table刷盘，已经level0 node插入

--- src/main/java/com/yumi/lsm/Tree.java
+++ src/main/java/com/yumi/lsm/Tree.java
@@ -1,21 +1,29 @@
 package com.yumi.lsm;
 
+import com.yumi.lsm.filter.BitsArray;
 import com.yumi.lsm.memtable.MemTable;
+import com.yumi.lsm.sst.Index;
 import com.yumi.lsm.sst.Node;
+import com.yumi.lsm.sst.SstReader;
+import com.yumi.lsm.sst.SstWriter;
 import com.yumi.lsm.util.AllUtils;
+import com.yumi.lsm.util.Kv;
 import com.yumi.lsm.wal.WalWriter;
+import org.jctools.queues.SpscArrayQueue;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Tree {
     private Config config;
-    private final ReadWriteLock dataLock = new ReentrantReadWriteLock();
+    private final ReentrantReadWriteLock dataLock = new ReentrantReadWriteLock();
     private int memTableIndex = 0;
     private WalWriter walWriter;
     private MemTable memtable;
@@ -23,9 +31,18 @@ public class Tree {
     private List<List<Node>> nodes;
     private ReentrantReadWriteLock[] levelLocks;
 
+    private SpscArrayQueue<MemTableCompactItem> memCompactQueue = new SpscArrayQueue<>(500);
+    private SpscArrayQueue<Integer> levelCompactQueue = new SpscArrayQueue<>(200);
+    private AtomicBoolean stop = new AtomicBoolean(false);
+    private AtomicInteger[] levelToSeq;
+
     public Tree(Config config) {
         this.config = config;
         this.nodes = new ArrayList<>();
+        this.levelToSeq = new AtomicInteger[config.getMaxLevel()];
+        for (int i = 0; i < levelToSeq.length; i++) {
+            levelToSeq[i] = new AtomicInteger(0);
+        }
         for (int i = 0; i < config.getMaxLevel(); i++) {
             nodes.add(new ArrayList<>());
         }
@@ -37,7 +54,7 @@ public class Tree {
     }
 
     public void put(byte[] key, byte[] value) {
-        Lock lock = dataLock.writeLock();
+        ReentrantReadWriteLock.WriteLock lock = dataLock.writeLock();
         lock.lock();;
         try {
             //1.写入wal file
@@ -63,9 +80,127 @@ public class Tree {
         //放到冷表队列中
         readOnlyMemTableList.add(oldItem);
         this.walWriter.close();
-        // TODO: 异步方式把冷表持久化为0层的sst文件
+        while (true) {
+            boolean offer = this.memCompactQueue.offer(oldItem);
+            if (offer) {
+                break;
+            }
+            Thread.yield();
+        }
         this.memTableIndex++;
         this.newMemTable(this.config.getWalFileSize());
+    }
+
+    private void doBackendTask() {
+        while (!stop.get()) {
+            MemTableCompactItem item = memCompactQueue.poll();
+            if (null != item) {
+                this.compactMemTable(item);
+            }
+        }
+    }
+
+    private void compactMemTable(MemTableCompactItem item) {
+        flushMemTable(item.memTable);
+        ReentrantReadWriteLock.WriteLock writeLock = this.dataLock.writeLock();
+        writeLock.lock();
+        try {
+            List<MemTableCompactItem> rms = this.readOnlyMemTableList;
+            int rmsSize = rms.size();
+            for (int i = 0; i < rmsSize; i++) {
+                if (rms.get(i).getMemTable() != item.getMemTable()) {
+                    new File(rms.get(i).getWalFile()).delete();
+                    continue;
+                }
+                this.readOnlyMemTableList = new ArrayList<>(rms.subList(i + 1, rmsSize));
+                new File(rms.get(i).getWalFile()).delete();
+                break;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    private void flushMemTable(MemTable memTable) {
+        int seq = levelToSeq[0].get() + 1;
+        SstWriter sstWriter = new SstWriter(sstFile(0, seq), config);
+        try {
+            for (Kv kv : memTable.all()) {
+                sstWriter.append(kv.getKey(), kv.getValue());
+            }
+            SstWriter.FinishRes finish = sstWriter.finish();
+            //插入内存表示node
+            insertNode(0, seq, finish.getSize(), finish.getBlockToFilter(), finish.getIndices());
+            //尝试发起下一层的压缩
+            tryCompactSst(0);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            sstWriter.close();
+        }
+    }
+
+    private void tryCompactSst(int level) {
+        if (level == this.nodes.size() - 1) {
+            //最后一层不压缩
+            return;
+        }
+        int size = 0;
+        for (Node node : this.nodes.get(level)) {
+            size += node.size();
+        }
+        if (size <= this.config.getLevelSstSize(level)) {
+            return;
+        }
+        boolean offer = this.levelCompactQueue.offer(level);
+        if (!offer) {
+            System.out.println("压缩文件正忙");
+        }
+    }
+
+    private void insertNode(int level, int seq, int size, Map<Integer, BitsArray> blockToFilter, Index[] indices) {
+        String file = sstFile(level, seq);
+        SstReader sstReader = new SstReader(file, config);
+        this.insertNodeWithReader(level, seq, size, blockToFilter, indices, sstReader);
+    }
+
+    private void insertNodeWithReader(int level, int seq, int size, Map<Integer, BitsArray> blockToFilter, Index[] indices, SstReader sstReader) {
+        String file = sstFile(level, seq);
+        this.levelToSeq[level].set(seq);
+        Node newNode = new Node(config, file, sstReader, size, blockToFilter, indices);
+        ReentrantReadWriteLock.WriteLock writeLock = this.levelLocks[level].writeLock();
+        writeLock.lock();
+        try {
+            if (level == 0) {
+                this.nodes.get(level).add(newNode);
+            } else {
+                List<Node> levelNodes = nodes.get(level);
+                if (levelNodes.isEmpty()) {
+                    levelNodes.add(newNode);
+                } else {
+                    int i = 0;
+                    for (; i < levelNodes.size(); i++) {
+                        if (AllUtils.compare(newNode.end(), levelNodes.get(i).start()) < 0) {
+                            levelNodes.add(i, newNode);
+                        }
+                    }
+                    if (i == levelNodes.size()) {
+                        levelNodes.add(newNode);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            writeLock.unlock();
+        }
+
+    }
+
+    private String sstFile(int level, int seq) {
+        return level + "_" + seq + ".sst";
     }
 
     private void newMemTable(int size) {
@@ -79,7 +214,7 @@ public class Tree {
 
 
     public byte[] get(byte[] key) {
-        Lock lock = this.dataLock.readLock();
+        ReentrantReadWriteLock.ReadLock lock = this.dataLock.readLock();
         lock.lock();
         try {
             //mem table找
